8e0b388bc9d18ab27a6ef365edef1c1a60345c32
Evan Ren <Evan@MacBook-Pro.local>
2020-10-30 02:36:52 -0700

    added stuff with scalers

--- logistic.py
+++ logistic.py
@@ -12,6 +12,17 @@ from sklearn.metrics import plot_confusion_matrix
 # matplotlib 3.3.1
 from matplotlib import pyplot
 
+import numpy as np
+
+#scalers
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import quantile_transform
+from sklearn.preprocessing import power_transform
+from sklearn.preprocessing import Normalizer
+
 #lol not gonna change the variable name each time
 boston = load_boston()
 bostonX = boston.data
@@ -20,8 +31,42 @@ for i in range(len(bostonY)):
     bostonY[i] = bostonY[i]//10
     #the value is already divided by 10,000 or 100,000 or whatever it is
 # print(max(bostonY), min(bostonY))
+
+# scaler = StandardScaler()
+# scaler.fit(bostonX)
+# scaledX = scaler.transform(bostonX)
+#Correct: 112, Incorrect: 40, % Correct:  0.74
+#this did not actually improve the prediction
+
+# transformer = MaxAbsScaler().fit(bostonX)
+# transformer.transform(bostonX)
+#Correct: 114, Incorrect: 38, % Correct:  0.75
+#also did not really do anything
+
+# scaler = MinMaxScaler()
+# scaler.fit(bostonX)
+# scaledX = scaler.transform(bostonX)
+#Correct: 111, Incorrect: 41, % Correct:  0.73
+
+# rng = np.random.RandomState(0)
+# bostonX = np.sort(rng.normal(loc=0.5, scale=0.25, size=(25, 1)), axis=0)
+# scaledX = quantile_transform(bostonX, n_quantiles=10, random_state=0, copy=True)
+# this one didnt work
+
+# transformer =  RobustScaler().fit(bostonX)
+# scaledX = transformer.transform(bostonX)
+# Correct: 107, Incorrect: 45, % Correct:   0.7
+
+# scaledX = power_transform(bostonX, method = 'box-cox')
+# also didnt work
+
+transformer = Normalizer().fit(bostonX)
+scaledX = transformer.transform(bostonX)
+#Correct: 92, Incorrect: 60, % Correct:  0.61
+#the rest didnt do anything, but this one made it worse
+
 trainX, testX, trainY, testY = train_test_split(
-    bostonX, bostonY, test_size = 0.3, shuffle = True
+    scaledX, bostonY, test_size = 0.3, shuffle = True
 )
 
 ### digits dataset:
